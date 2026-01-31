bbdf40c15b0be89f50deec951e88d09889e00025
GaiusRed <erik@gaius.red>
2026-01-31 18:16:05 +0800

    Initial commit.

---
+++ common/src/main/java/red/gaius/brightbronze/ExampleMod.java
@@ -0,0 +1,9 @@
+package red.gaius.brightbronze;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "brightbronze_horizons";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
